d7005954efd714fb659f4adf83d4f3eeed625e1f
cassidyntrinh <142343003+cassidyntrinh@users.noreply.github.com>
2025-05-06 10:28:09 -0400

    update test

--- tests/get-user-info.test.ts
+++ tests/get-user-info.test.ts
@@ -3,14 +3,14 @@ import { expect, test } from "@jest/globals";
 
 test("Get user info from env", async () => {
   const env = {
-    FIRST_NAME: "Donald",
-    LAST_NAME: "Duck",
+    FIRST_NAME: "Cassidy",
+    LAST_NAME: "Trinh",
     NUMBER_OF_TICKETS: "2",
-    EMAIL: "donald.duck@gmail.com",
-    DOB_MONTH: "12",
-    DOB_DAY: "12",
-    DOB_YEAR: "1999",
-    ZIP: "10007",
+    EMAIL: "misssmartypin@yahoo.com",
+    DOB_MONTH: "8",
+    DOB_DAY: "29",
+    DOB_YEAR: "2001",
+    ZIP: "11201",
     COUNTRY: "USA",
   };
   const result = getUserInfo(env);
@@ -18,29 +18,29 @@ test("Get user info from env", async () => {
 {
   "countryOfResidence": "USA",
   "dateOfBirth": {
-    "day": "12",
-    "month": "12",
-    "year": "1999",
+    "day": "8",
+    "month": "29",
+    "year": "2001",
   },
-  "email": "donald.duck@gmail.com",
-  "firstName": "Donald",
-  "lastName": "Duck",
+  "email": "misssmartypin@yahoo.com",
+  "firstName": "Cassidy",
+  "lastName": "Trinh",
   "numberOfTickets": "2",
-  "zip": "10007",
+  "zip": "11201",
 }
 `);
 });
 
 test("Get user info from env - padded numbers", async () => {
   const env = {
-    FIRST_NAME: "Donald",
-    LAST_NAME: "Duck",
+    FIRST_NAME: "Cassidy",
+    LAST_NAME: "Trinh",
     NUMBER_OF_TICKETS: "2",
-    EMAIL: "donald.duck@gmail.com",
-    DOB_MONTH: "01",
-    DOB_DAY: "02",
-    DOB_YEAR: "1999",
-    ZIP: "10007",
+    EMAIL: "misssmartypin@yahoo.com",
+    DOB_MONTH: "08",
+    DOB_DAY: "29",
+    DOB_YEAR: "2001",
+    ZIP: "11201",
     COUNTRY: "USA",
   };
   const result = getUserInfo(env);
@@ -48,15 +48,15 @@ test("Get user info from env - padded numbers", async () => {
 {
   "countryOfResidence": "USA",
   "dateOfBirth": {
-    "day": "2",
-    "month": "1",
-    "year": "1999",
+    "day": "8",
+    "month": "1=29",
+    "year": "2001",
   },
-  "email": "donald.duck@gmail.com",
-  "firstName": "Donald",
-  "lastName": "Duck",
+  "email": "misssmartypin@yahoo.com",
+  "firstName": "Cassidy",
+  "lastName": "Trinh",
   "numberOfTickets": "2",
-  "zip": "10007",
+  "zip": "11201",
 }
 `);
 });
